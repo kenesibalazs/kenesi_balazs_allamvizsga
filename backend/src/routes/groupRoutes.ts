7d9029e23c254bc87d505eb86eef6fa3ca6326e6
kenesibalazs <125258978+kenesibalazs@users.noreply.github.com>
2024-08-14 13:52:09 +0300

    groupValidator

--- backend/src/routes/groupRoutes.ts
+++ backend/src/routes/groupRoutes.ts
@@ -1,5 +1,12 @@
 import express from 'express';
 import { GroupController } from '../controllers/groupController';
+import { validateRequest } from '../middleware/validationMiddleware';
+import {
+    createGroupValidator,
+    updateGroupValidator
+}   from '../validators/groupValidator';
+
+
 
 const app = express.Router();
 const groupController = new GroupController();
@@ -7,8 +14,10 @@ const groupController = new GroupController();
 app.get('/groups', groupController.getAllGroups.bind(groupController)); // Get all groups
 app.get('/groups/majors/:majorId', groupController.getGroupsByMajorId.bind(groupController)); // Get groups by major ID
 app.get('/groups/:id', groupController.getGroupById.bind(groupController));
-app.post('/groups', groupController.createGroup.bind(groupController));
-app.put('/groups/:id', groupController.updateGroup.bind(groupController));
+
+app.post('/groups', validateRequest(createGroupValidator), groupController.createGroup.bind(groupController));
+app.put('/groups/:id',validateRequest(updateGroupValidator), groupController.updateGroup.bind(groupController));
+
 app.delete('/groups/:id', groupController.deleteGroup.bind(groupController));
 
 export default app;
--- backend/src/validators/groupValidator.ts
+++ backend/src/validators/groupValidator.ts
@@ -1,2 +1,12 @@
 import Joi from 'joi';
 
+export const createGroupValidator = Joi.object({
+    name: Joi.string().required(),
+    majorId: Joi.string().required(),
+});
+
+export const updateGroupValidator = Joi.object({
+    name: Joi.string(),
+    majorId: Joi.string(),
+})
+
