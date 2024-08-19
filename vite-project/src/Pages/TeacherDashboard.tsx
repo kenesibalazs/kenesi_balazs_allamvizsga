2766a1cfde922ab4169946a1b7663efbff243f3a
kenesibalazs <125258978+kenesibalazs@users.noreply.github.com>
2024-08-19 17:56:09 +0300

    eslint

--- vite-project/src/Pages/TeacherDashboard.tsx
+++ vite-project/src/Pages/TeacherDashboard.tsx
@@ -259,7 +259,7 @@ const TeacherDashboard: React.FC<TeacherDashboardProps> = ({ userData }) => {
 
                             <div className="chartContainer">
                                 <LineChart
-                                    xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] }]}
+                                    xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13,14,15,16] }]}
                                     series={[
                                         {
                                             data: chartData.map((data) => data.count),
